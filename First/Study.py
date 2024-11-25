7121248847b82d7c9a8133506b3d6397f523b732
Leonardo Avila Morales <leoavila0695@gmail.com>
2024-11-25 13:53:40 -0500

    Just added a "print item by item" formula

--- First/Study.py
+++ First/Study.py
@@ -249,3 +249,16 @@
 #   return max(evens)
 
 # print(highest_even([2,10,2,3,4,8,11]))
+
+
+# PRINT ITEM BY ITEM 
+# a = 'helloooooooooo'
+
+# if ((n := len(a)) > 10):
+#     print(f"too long {n} elements")
+
+# while ((n := len(a)) > 1):
+#     print(n)
+#     a = a[:-1]
+
+# print(a)
