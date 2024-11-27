8516b8b659c479c24c6259c75feba75d29219150
Leonardo Avila Morales <leoavila0695@gmail.com>
2024-11-27 12:59:03 -0500

    First practical case

--- First/Java/HolaMundo.java
+++ First/Java/HolaMundo.java
@@ -1,5 +1,39 @@
-class HolaMundo {
+// class HolaMundo {
+//     public static void main(String[] args) {
+
+//         int num = 15;
+//         System.out.println("this is your number = " + num);
+//     }
+// }
+
+
+
+import java.util.Scanner;
+class CasoPractico1 {
     public static void main(String[] args) {
-        System.out.println("¡Hola, mundo! Este es mi primer programa en Java.");
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Please write the first number: ");
+        int a = input.nextInt();
+        
+        System.out.print("Please write the second number: ");
+        int b = input.nextInt();
+        
+        System.out.print("Please write the third number: ");
+        float c = input.nextFloat();
+
+        input.nextLine();
+
+        System.out.print("Please write text 1: ");
+        String d = input.nextLine();
+
+        System.out.print("Please write text 2: ");
+        String e = input.nextLine();
+
+        System.out.println ("the sum of the first 3 numbers is: " + (a+b+c));
+        System.out.print("first text plus second = " + d + e);
+
+        input.close();
     }
 }
+
