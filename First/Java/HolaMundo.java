ee0dc65ce3d8657b0e48c2276f97644265a3c589
Leonardo Avila Morales <leoavila0695@gmail.com>
2024-11-28 12:28:27 -0500

    Caso Practico 3 Answer

--- First/Java/HolaMundo.java
+++ First/Java/HolaMundo.java
@@ -63,23 +63,54 @@
 // SOLUCION CASO PRACTICO 2
 // RANDOM NUMBER
 
+// import java.util.Scanner;
+// import java.util.Random;
+// class CasoPractico2{
+//     public static void main(String[] args){
+//         Scanner input = new Scanner(System.in);
+//         Random random =new Random();
+
+//         System.out.println("Please write how many numbers do you want in the following random print list: ");
+//         int num = input.nextInt();
+
+
+//         int i = 1;
+//         while (i <= num){
+//             int randomInt = random.nextInt(65 - 18 + 1) + 18;
+//             System.out.println("This is your "+i+" random number between 18 and 65: " + randomInt);
+//             i ++;
+//         }
+//         input.close();
+//     }
+// }
+
+// SOLUCION CASO PRACTICO 3
+// RANDOM NUMBER
+
 import java.util.Scanner;
-import java.util.Random;
-class CasoPractico2{
+
+class CasoPractico3{
     public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
-        Random random =new Random();
+    Scanner input = new Scanner(System.in);
 
-        System.out.println("Please write how many numbers do you want in the following random print list: ");
-        int num = input.nextInt();
+    char letras[] = { 'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P',
+    'D', 'X', 'B', 'N', 'J', 'Z', 'S','Q','V', 'H', 'L', 'C', 'K', 'E' };
 
+    // int dni [];
+    System.out.println("Type your DNI number: ");
+    int num = input.nextInt();
+    int size = String.valueOf(num).length();
 
-        int i = 1;
-        while (i <= num){
-            int randomInt = random.nextInt(65 - 18 + 1) + 18;
-            System.out.println("This is your "+i+" random number between 18 and 65: " + randomInt);
-            i ++;
-        }
-        input.close();
+    if (size == 8){
+        System.out.println("This is your number: " + num + " and size: " + size);
+        int mod = num % 23;
+        char letter = letras[mod];
+        System.out.println("This is your number: " + num + letter);
+    }
+
+    else{
+    System.out.println("This is not a valid number...");
+    }
+
     }
 }
