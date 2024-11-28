9f4ff4ba78ea9dc92e8f174f1d3b16f5a171000d
Leonardo Avila Morales <leoavila0695@gmail.com>
2024-11-28 08:54:56 -0500

    Caso Practico 2 solution(random in a range)

--- First/Java/HolaMundo.java
+++ First/Java/HolaMundo.java
@@ -7,55 +7,79 @@
 // }
 
 
+//SOLUCION CASO PRACTICO 1
+// INPUT INTEGER, FLOAT, STRING
+// import java.util.Scanner;
+// class CasoPractico1 {
+//     public static void main(String[] args) {
+//         Scanner input = new Scanner(System.in);
+// // getting variables
+//         System.out.print("Please write the first number: ");
+//         int a = input.nextInt();
+        
+//         System.out.print("Please write the second number: ");
+//         int b = input.nextInt();
+        
+//         System.out.print("Please write the third number: ");
+//         float c = input.nextFloat();
+
+//         input.nextLine();
+
+//         System.out.print("Please write text 1: ");
+//         String d = input.nextLine();
+
+//         System.out.print("Please write text 2: ");
+//         String e = input.nextLine();
+
+//         // Print the values
+//         System.out.println ("the sum of the first 3 numbers is: " + (a+b+c));
+
+//         // conditions to know the biggest number
+//         if (a > b && a > c) {
+//             System.out.println("the biggest number is: " + a);
+//         }
+//         else if (b > a && b > c) {
+//             System.out.println("the biggest number is: " + b);
+//         }
+//         else if (c > a && c > b) {
+//             System.out.println("the biggest number is: " + c);
+//         }
+
+//         // Division among float and Ints
+//         float div1 = a / c;
+//         float div2 = b / c;
+
+//         System.out.println("First division among " + a + " and " + c + "= " + div1);
+//         System.out.println("First division among " + b + " and " + c + "= " + div2);
+
+//         //Concatenation of String lines
+//         System.out.println("first text plus second = " + d + " " + e);
+
+
+//         input.close();
+//     }
+// }
+
+// SOLUCION CASO PRACTICO 2
+// RANDOM NUMBER
 
 import java.util.Scanner;
-class CasoPractico1 {
-    public static void main(String[] args) {
+import java.util.Random;
+class CasoPractico2{
+    public static void main(String[] args){
         Scanner input = new Scanner(System.in);
-// getting variables
-        System.out.print("Please write the first number: ");
-        int a = input.nextInt();
-        
-        System.out.print("Please write the second number: ");
-        int b = input.nextInt();
-        
-        System.out.print("Please write the third number: ");
-        float c = input.nextFloat();
+        Random random =new Random();
 
-        input.nextLine();
+        System.out.println("Please write how many numbers do you want in the following random print list: ");
+        int num = input.nextInt();
 
-        System.out.print("Please write text 1: ");
-        String d = input.nextLine();
 
-        System.out.print("Please write text 2: ");
-        String e = input.nextLine();
-
-        // Print the values
-        System.out.println ("the sum of the first 3 numbers is: " + (a+b+c));
-
-        // conditions to know the biggest number
-        if (a > b && a > c) {
-            System.out.println("the biggest number is: " + a);
+        int i = 1;
+        while (i <= num){
+            int randomInt = random.nextInt(65 - 18 + 1) + 18;
+            System.out.println("This is your "+i+" random number between 18 and 65: " + randomInt);
+            i ++;
         }
-        else if (b > a && b > c) {
-            System.out.println("the biggest number is: " + b);
-        }
-        else if (c > a && c > b) {
-            System.out.println("the biggest number is: " + c);
-        }
-
-        // Division among float and Ints
-        float div1 = a / c;
-        float div2 = b / c;
-
-        System.out.println("First division among " + a + " and " + c + "= " + div1);
-        System.out.println("First division among " + b + " and " + c + "= " + div2);
-
-        //Concatenation of String lines
-        System.out.println("first text plus second = " + d + " " + e);
-
-
         input.close();
     }
 }
-
