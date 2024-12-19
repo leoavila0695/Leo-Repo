386edff7aeccc915752390306244b90c9b201e96
Leonardo Avila Morales <leoavila0695@gmail.com>
2024-12-19 13:57:27 -0500

    Courseware project initiation

---
+++ First/Java/New_task.java
@@ -0,0 +1,15 @@
+public class New_task {
+    public New_task() {
+        System.out.print("Hello world");
+        System.out.println(hello());
+    }
+
+    public String hello(){
+        return " and so...";
+    }
+
+    public static void main(String[] var0){
+        System.out.println("My class began...");
+        New_task greet = new New_task();
+    }
+}
