3920bb3c54268b0673d872083f3a7c0a5f173353
Leonardo Avila Morales <leoavila0695@gmail.com>
2025-01-09 01:36:17 -0500

    super() and introspection

--- First/Python/Study.py
+++ First/Python/Study.py
@@ -431,58 +431,68 @@
 
 
 
-# Cats_everywhere exercise:
-class Pets:
-    animals = []
-    def __init__(self, animals):
-        self.animals = animals
+# # Cats_everywhere exercise:
+# class Pets:
+#     animals = []
+#     def __init__(self, animals):
+#         self.animals = animals
 
-    def walk(self):
-        for animal in self.animals:
-            print(animal.walk())
+#     def walk(self):
+#         for animal in self.animals:
+#             print(animal.walk())
 
-class Cat:
-    is_lazy = True
+# class Cat:
+#     is_lazy = True
 
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
 
-    def walk(self):
-        return f'{self.name} is just walking around'
+#     def walk(self):
+#         return f'{self.name} is just walking around'
 
-class Simon(Cat):
-    def sing(self, sounds):
-        return f'{sounds}'
+# class Simon(Cat):
+#     def sing(self, sounds):
+#         return f'{sounds}'
 
-class Sally(Cat):
-    def sing(self, sounds):
-        return f'{sounds}'
+# class Sally(Cat):
+#     def sing(self, sounds):
+#         return f'{sounds}'
 
-#1 Add nother Cat DONE
-class Aren(Cat):
-    def sing(self, sounds):
-        return f'{sounds}'
-
-
+# #1 Add nother Cat DONE
+# class Aren(Cat):
+#     def sing(self, sounds):
+#         return f'{sounds}'
 
 
-#2 Create a list of all of the pets (create 3 cat instances from the above) DONE
-my_cats = []
-simon = Simon("Simon",6)
-sally = Sally("Sally",5)
-aren = Aren("Aren",12)
-
-my_cats.append(simon)
-my_cats.append(sally)
-my_cats.append(aren)
 
 
-#3 Instantiate the Pet class with all your cats use variable my_pets DONE
-my_pets = Pets(my_cats)
+# #2 Create a list of all of the pets (create 3 cat instances from the above) DONE
+# my_cats = []
+# simon = Simon("Simon",6)
+# sally = Sally("Sally",5)
+# aren = Aren("Aren",12)
 
-#4 Output all of the cats walking using the my_pets instance
+# my_cats.append(simon)
+# my_cats.append(sally)
+# my_cats.append(aren)
 
-my_pets.walk()
 
+# #3 Instantiate the Pet class with all your cats use variable my_pets DONE
+# my_pets = Pets(my_cats)
 
+# #4 Output all of the cats walking using the my_pets instance
+
+# my_pets.walk()
+
+# class Wizard(User):
+#   def __init__(self, name, power, email):
+#     super().__init__(email)
+#     self.name = name
+#     self.power = power
+    
+
+# introsprection
+
+# wizard1 = Wizard('Merlin', 60, 'merlin@gmail.com')
+# print(dir(wizard1))
